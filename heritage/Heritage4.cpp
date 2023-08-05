e936e5aebef2f0b0d07c9ee2b2d302020309404e
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-05 18:01:06 +0530

    Update Heritage4.cpp

--- heritage/Heritage4.cpp
+++ heritage/Heritage4.cpp
@@ -1,77 +1,259 @@
-#include <iostream>
-#include <stdio.h>
+#include<iostream>
+
+#include<string.h>
 using namespace std;
 
-class emplooyinfo {
-protected:
-    char name[30];
-    int empId;
-    char gender;
+class Employee{
+public :
+int id;
+char name[100];
+char role[100];
+int salary;
+int exp;
+char company[100];
+char address[100];
+char email[100];
+char contact[10];
 
-public:
-    void getemplooyinfo (void)
-    {
-        cout << "Enter Name: ";
-        cin.getline(name, 30);
-        cout << "Enter Emp. Id: ";
-        cin >> empId;
-        cout << "Enter Gender: ";
-        cin >> gender;
-    }
+
+	void setter(){
+      cout<<"enter id =";
+      cin>>id;
+      cout<<"enter name =";
+      cin>>name;
+      cout<<"enter job role =";
+      cin>>role;
+      cout<<endl;	
+
+	}
+
 };
 
-class deptInfo {
-protected:
-    char deptName[30];
-    char assignedWork[30];
-    int time2complete;
 
-public:
-    void getDeptInfo(void)
-    {
-        cout << "Enter Department Name: ";
-        cin.ignore(1);
-        cin.getline(deptName, 30);
-        cout << "Enter assigned work: ";
-        fflush(stdin);
-        cin.getline(assignedWork, 30);
-        cout << "Enter time in hours to complete work: ";
-        cin >> time2complete;
-    }
+
+
+class Employee2 : public Employee{
+public :
+int id;
+char name[100];
+char role[100];
+int salary;
+int exp;
+char company[100];
+char address[100];
+char email[100];
+char contact[10];
+
+void setter(){
+cout<<"enter id =";
+cin>>id;
+cout<<"enter name =";
+cin>>name;
+cout<<"enter job role =";
+cin>>role;
+cout<<"enter salary =";
+cin>>salary;
+cout<<"enter company =";
+cin>>company;
+cout<<"contact =";
+cin>>contact;
+cout<<endl;
+
+
+	}
 };
 
-class employee : private emplooyinfo , private deptInfo {
-public:
-    void getEmployeeInfo(void)
-    {
-        cout << "Enter employee's basic info: " << endl;
-        
-        getBasicInfo();
-        cout << "Enter employee's department info: " << endl;
-        getDeptInfo(); 
-    }
-    void printEmployeeInfo(void)
-    {
-        cout << "Employee's Information is: " << endl;
-        cout << "Basic Information...:" << endl;
-        cout << "Name: " << name << endl; 
-        cout << "Employee ID: " << empId << endl;
-        cout << "Gender: " << gender << endl
-             << endl; 
 
-        cout << "Department Information...:" << endl;
-        cout << "Department Name: " << deptName << endl; 
-        cout << "Assigned Work: " << assignedWork << endl; 
-        cout << "Time to complete work: " << time2complete << endl; 
-    }
+
+
+class Employee3 : public Employee2{
+
+
+
+
+	public :
+
+
+
+
+	int id;
+
+	char name[100];
+
+	char role[100];
+
+	int salary;
+
+	int exp;
+
+	char company[100];
+
+	char address[100];
+
+	char email[100];
+
+	char contact[10];
+
+
+
+
+	void setter(){
+
+
+
+
+	    cout<<"enter company name =";
+
+		cin>>company;
+
+
+
+
+		cout<<"enter address =";
+
+		cin>>address;
+
+
+
+
+		cout<<endl;
+
+
+
+
+	}
+
+
+
+
+	void getter(){
+
+
+
+
+		cout<<"enter name ="<<name<<endl;
+
+		cout<<"enter role ="<<role<<endl;
+
+		cout<<"enter salary ="<<salary<<endl;
+
+
+
+
+		}
+
+
+
+
 };
 
-int main()
-{
-    employee emp;
 
-    emp.getEmployeeInfo();
-    emp.printEmployeeInfo();
 
-    return 0;
+
+class Employee4 : public Employee3{
+
+
+
+
+	public :
+
+
+
+
+	int id;
+
+	char name[100];
+
+	char role[100];
+
+	int salary;
+
+	int exp;
+
+	char company[100];
+
+	char address[100];
+
+	char email[100];
+
+	char contact[10];
+
+
+
+
+	void setter(){
+    cout<<"enter email =";
+
+		cin>>email;
+
+     	cout<<"enter contact =";
+
+		cin>>contact;	
+
+
+
+
+	}
+
+
+	void getter(){
+        cout<<"enter id ="<<id<<endl;	
+
+		cout<<"enter name ="<<name<<endl;
+
+		cout<<"enter role ="<<role<<endl;
+
+		cout<<"enter salary ="<<salary<<endl;
+
+		cout<<"enter experience ="<<exp<<endl;
+
+		cout<<"enter company name ="<<company<<endl;
+
+		cout<<"enter address ="<<address<<endl;
+
+		cout<<"enter email ="<<email<<endl;
+
+		cout<<"enter contact ="<<contact<<endl;	
+
+
+
+
+		cout<<endl;
+
+	}
+
+};
+
+
+
+
+int main(){
+
+
+
+
+	Employee4 e;
+  e.Employee::setter();
+
+    e.Employee2::setter();
+
+    e.Employee3::setter();
+
+    e.Employee4::setter();
+
+
+
+
+	e.Employee3::getter();
+
+	e.Employee4::getter();
+
+
+
+
+	return 0;
+
+
+
+
 }
