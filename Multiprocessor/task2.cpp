622552a1771fe86e0fbd19f1635d410a91e5a8d3
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-08-07 16:53:14 +0530

    Update task2.cpp

--- Multiprocessor/task2.cpp
+++ Multiprocessor/task2.cpp
@@ -1,23 +1,58 @@
 #include<iostream>
 #include<string.h>
+
 using namespace std;
 
-class MyClass {
-public:
-    void memberFunc(int a = 0, int b = 0, int c = 0, int d = 0) {
-        cout << " arguments: "
-		     << a << " " << b << " " << c << " " << d << endl;
-    }
+class method{
+	
+	public :
+		
+		int a;
+		float b;
+		char c[100];
+		
+		void set(){
+			cout<<"Good Morning"<<endl;
+			cout<<endl;
+			
+		}
+		void set(int a){
+			this->a=a;
+			cout<<"value of a ="<<a<<endl;
+			
+			
+		}
+		void set(int a,float b){
+			this->a=a;
+			this->b=b;
+			cout<<"value of a ="<<a<<endl;
+			cout<<"value of b ="<<b<<endl;
+			
+			cout<<endl;
+			
+		}
+			void set(int a,float b,char c[]){
+			this->a=a;
+			this->b=b;
+			strcpy(this->c,c);
+			cout<<"value of a ="<<a<<endl;
+			cout<<"value of b ="<<b<<endl;
+			cout<<"value of c ="<<c<<endl;
+			
+			cout<<endl;
+			
+		}
+		
 };
 
-int main() {
-    MyClass obj;
-
-    obj.memberFunc();
-    obj.memberFunc(1);
-    obj.memberFunc(1, 2);
-    obj.memberFunc(1, 2, 3);
-    obj.memberFunc(1, 2, 3, 4);
-
-    return 0;
+int main(){
+	
+	method m;
+	
+	m.set();
+	m.set(2119);
+	m.set(326,12.26);
+	m.set(33,13.56,"hi");
+	
+	return 0;
 }
